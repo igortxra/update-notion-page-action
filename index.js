fb19de025c202cf06dc389820d99cd5f79dbaf6c
igortxra <igorxteixeira+github@gmail.com>
2022-09-12 22:00:40 -0300

    Turn action in notion page updater

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { Client } = require('@notionhq/client');
+
+const NOTION_TOKEN = core.getInput('notion-token')
+const PAGE_ID = core.getInput('page-id')
+const notion = new Client({ auth: NOTION_TOKEN });
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  (async () => {
+    const response = await notion.pages.update({
+      page_id: PAGE_ID,
+      properties: {
+        'test coverage': {
+          number: 1.0,
+        },
+      },
+    });
+    console.log(response);
+  })();
+  core.setOutput("status", "success");
 } catch (error) {
+  core.setOutput("status", "error");
   core.setFailed(error.message);
 }
